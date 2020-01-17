f5d2374946849e3d80f5c5865dc34f4ef3c3527d
SparhawkTheArtist <sparhawktheartist@gmail.com>
2020-01-17 13:28:07 -0800

    modular nav

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,28 @@ function lightmode (){
     }
 };
 
+
+//nav.html
+$(function(){
+    $("#nav-placeholder").append('<aside class="navMenu"><input id="navToggle" name="navToggle" type="checkbox"><label for="navToggle">Navigation</label><img src="/img/sparhawk.png" class="logo" alt="Sparhawk Logo"><ul><li class="nav1"><a href="/index.html">About</a></li><li class="nav2"><a href="/desn.html">Design</a></li><li><a href="/motn.html">Motion</a></li><li class="nav4"><a href="/ills.html">Illustration</a></li><li><a href="/foto.html">Photography</a></li></ul></aside>');  
+    if ($('#title').text() == ('Illustration')){
+        $('.nav4').addClass('activePage');
+    };
+    if ($('#title').text() == ('Design')){
+        $('.nav2').addClass('activePage');
+    };
+            
+    
+});
+
+
+console.log($('#title').text());
+
+$(function(){
+    $("#foot-placeholder").load("footer.txt");
+});
+
+
 // at page load, check for lightmode cookie
 if (Cookies.get('lightmode') == ('on')){
     $('#checkbox').attr('checked', true);
